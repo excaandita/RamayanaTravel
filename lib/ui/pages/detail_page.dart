445619e486d4fb0fb15c765b6befe2d4310dcb03
Exca Muchlis Andita <excamuchlisandita@gmail.com>
2022-08-17 11:06:38 +0700

    Detail Page

--- lib/ui/pages/detail_page.dart
+++ lib/ui/pages/detail_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:travelapp/shared/theme.dart';
+import 'package:travelapp/ui/widgets/interest_item.dart';
+import 'package:travelapp/ui/widgets/photo_item.dart';
 
 class DetailPage extends StatelessWidget {
   const DetailPage({Key? key}) : super(key: key);
@@ -37,12 +39,193 @@ class DetailPage extends StatelessWidget {
       );
     }
 
+    Widget content() {
+      return Container(
+        width: double.infinity,
+        margin: EdgeInsets.symmetric(
+          horizontal: defaultMargin,
+        ),
+        child: Column(
+          children: [
+            //Note : Emblem
+            Container(
+              width: 108,
+              height: 24,
+              margin: EdgeInsets.only(top: 30),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/icon_emblem.png'),
+                ),
+              ),
+            ),
+
+            //Note : Title
+            Container(
+              margin: EdgeInsets.only(top: 256),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Candi Borobudur',
+                          style: whiteTextStyle.copyWith(
+                            fontSize: 24,
+                            fontWeight: semiBold,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        Text(
+                          'Magelang',
+                          style: whiteTextStyle.copyWith(
+                            fontSize: 16,
+                            fontWeight: light,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        width: 20,
+                        height: 20,
+                        margin: EdgeInsets.only(right: 2),
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage('assets/icon_star.png'),
+                          ),
+                        ),
+                      ),
+                      Text(
+                        '4.7',
+                        style: whiteTextStyle.copyWith(
+                          fontSize: 14,
+                          fontWeight: semiBold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+
+            //Note : About
+            Container(
+              width: double.infinity,
+              margin: EdgeInsets.only(top: 30),
+              padding: EdgeInsets.symmetric(
+                vertical: 30,
+                horizontal: 20,
+              ),
+              decoration: BoxDecoration(
+                color: kWhiteColor,
+                borderRadius: BorderRadius.circular(18),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'About',
+                    style: blackTextStyle.copyWith(
+                      fontSize: 16,
+                      fontWeight: semiBold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 6,
+                  ),
+                  Text(
+                    'Candi Borobudur adalah sebuah candi Buddha yang terletak di Borobudur, Magelang, Jawa Tengah, Indonesia. Candi ini terletak kurang lebih 100 km di sebelah barat daya Semarang, 86 km di sebelah barat Surakarta, dan 40 km di sebelah barat laut Yogyakarta',
+                    style: blackTextStyle.copyWith(
+                      height: 2.0,
+                    ),
+                  ),
+
+                  //note : photos
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    'Photo',
+                    style: blackTextStyle.copyWith(
+                      fontSize: 16,
+                      fontWeight: semiBold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 6,
+                  ),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                        PhotoItem(
+                          imageUrl: 'assets/image_destination2.png',
+                        ),
+                        PhotoItem(
+                          imageUrl: 'assets/image_destination3.png',
+                        ),
+                        PhotoItem(
+                          imageUrl: 'assets/image_destination5.png',
+                        ),
+                        PhotoItem(
+                          imageUrl: 'assets/image_destination1.png',
+                        ),
+                        PhotoItem(
+                          imageUrl: 'assets/image_destination4.png',
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  //note: interest
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    'Interest',
+                    style: blackTextStyle.copyWith(
+                      fontSize: 16,
+                      fontWeight: semiBold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      InterestItem(text: 'Kids Park'),
+                      InterestItem(text: 'City Museum'),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      InterestItem(text: 'Honor Bridge'),
+                      InterestItem(text: 'Central Mall'),
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
       backgroundColor: kBackgroundColor,
       body: Stack(
         children: [
           backgroundImage(),
           customShadow(),
+          content(),
         ],
       ),
     );
