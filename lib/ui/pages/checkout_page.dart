df2fd5788db4b47bee8bc99c349287cb66afe347
Exca Muchlis Andita <excamuchlisandita@gmail.com>
2022-08-17 14:31:33 +0700

    details Text

--- lib/ui/pages/checkout_page.dart
+++ lib/ui/pages/checkout_page.dart
@@ -1,13 +1,221 @@
 import 'package:flutter/material.dart';
 import 'package:travelapp/shared/theme.dart';
+import 'package:travelapp/ui/widgets/booking_details_item.dart';
 
 class CheckoutPage extends StatelessWidget {
   const CheckoutPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    Widget route() {
+      return Container(
+        margin: EdgeInsets.only(top: 50),
+        child: Column(
+          children: [
+            Container(
+              width: 291,
+              height: 65,
+              margin: EdgeInsets.only(bottom: 10),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/image_checkout.png'),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'SLO',
+                      style: blackTextStyle.copyWith(
+                        fontSize: 24,
+                        fontWeight: semiBold,
+                      ),
+                    ),
+                    Text(
+                      'Surakarta',
+                      style: greyTextStyle.copyWith(
+                        fontWeight: light,
+                      ),
+                    )
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      'MGL',
+                      style: blackTextStyle.copyWith(
+                        fontSize: 24,
+                        fontWeight: semiBold,
+                      ),
+                    ),
+                    Text(
+                      'Magelang',
+                      style: greyTextStyle.copyWith(
+                        fontWeight: light,
+                      ),
+                    )
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+      );
+    }
+
+    Widget bookingDetails() {
+      return Container(
+        margin: EdgeInsets.only(top: 30),
+        padding: EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 30,
+        ),
+        decoration: BoxDecoration(
+          color: kWhiteColor,
+          borderRadius: BorderRadius.circular(18),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //Note : Destination Tile
+            Row(
+              children: [
+                Container(
+                  width: 70,
+                  height: 70,
+                  margin: EdgeInsets.only(right: 16),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(18),
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage('assets/image_destination2.png'),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Terminal Magelang',
+                        style: blackTextStyle.copyWith(
+                          fontSize: 18,
+                          fontWeight: medium,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        'Magelang',
+                        style: greyTextStyle.copyWith(
+                          fontSize: 14,
+                          fontWeight: light,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: 20,
+                      height: 20,
+                      margin: EdgeInsets.only(right: 2),
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/icon_star.png'),
+                        ),
+                      ),
+                    ),
+                    Text(
+                      '4.1',
+                      style: blackTextStyle.copyWith(
+                        fontSize: 14,
+                        fontWeight: medium,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+
+            //Note : Detail Text
+            Container(
+              margin: EdgeInsets.only(top: 30),
+              child: Text(
+                'Booking Details',
+                style: blackTextStyle.copyWith(
+                  fontWeight: semiBold,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+
+            //note : Detail item
+            BookingDetailsItem(
+              title: 'Traveler',
+              valueColor: kBlackColor,
+              valueText: '2 Person',
+            ),
+            BookingDetailsItem(
+              title: 'Seat',
+              valueColor: kBlackColor,
+              valueText: 'A3, B3',
+            ),
+            BookingDetailsItem(
+              title: 'Insurance',
+              valueColor: kGreenColor,
+              valueText: 'Yes',
+            ),
+            BookingDetailsItem(
+              title: 'Refundable',
+              valueColor: kRedColor,
+              valueText: 'No',
+            ),
+            BookingDetailsItem(
+              title: 'VAT',
+              valueColor: kBlackColor,
+              valueText: '45 %',
+            ),
+            BookingDetailsItem(
+              title: 'Price',
+              valueColor: kBlackColor,
+              valueText: 'IDR 8.100.000',
+            ),
+            BookingDetailsItem(
+              title: 'Grand Total',
+              valueColor: kPrimaryColor,
+              valueText: 'IDR 11.850.000',
+            ),
+          ],
+        ),
+      );
+    }
+
+    Widget paymentDetails() {
+      return Container();
+    }
+
     return Scaffold(
       backgroundColor: kBackgroundColor,
+      body: ListView(
+        padding: EdgeInsets.symmetric(
+          horizontal: defaultMargin,
+        ),
+        children: [
+          route(),
+          bookingDetails(),
+          paymentDetails(),
+        ],
+      ),
     );
   }
 }
