9440e43adbafe9541c0810b44f359ffe51e384d6
Exca Muchlis Andita <excamuchlisandita@gmail.com>
2022-08-18 21:16:22 +0700

    Make:Model destination_model

---
+++ lib/models/destination_model.dart
@@ -0,0 +1,35 @@
+import 'package:equatable/equatable.dart';
+
+class DestinationModel extends Equatable {
+  final String id;
+  final String name;
+  final String city;
+  final String imageUrl;
+  final double rating;
+  final int price;
+  final String about;
+
+  DestinationModel({
+    required this.id,
+    this.name = '',
+    this.city = '',
+    this.imageUrl = '',
+    this.rating = 0.0,
+    this.price = 0,
+    this.about = '',
+  });
+
+  factory DestinationModel.fromJson(String id, Map<String, dynamic> json) =>
+      DestinationModel(
+        id: id,
+        name: json['name'],
+        city: json['city'],
+        imageUrl: json['imageUrl'],
+        rating: json['rating'],
+        price: json['price'],
+        about: json['about'],
+      );
+
+  @override
+  List<Object?> get props => [id, name, city, imageUrl, rating, price, about];
+}
